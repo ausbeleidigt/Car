5a05aa9264cae4e81cfa9f465312e445ec5fa512
Noah <nk-06@gmx.de>
2022-08-18 12:33:08 +0200

    Initial sharing of project

---
+++ Car.java
@@ -0,0 +1,20 @@
+public class Car{
+    String carDesc;
+    double fuelCap;
+    double fuel;
+    double fuelConsum;
+    double maxRange;
+    int drivenKm;
+    
+    
+    public Car(String carDesc, double fuelCap, double fuelConsum, double maxRange){
+        
+    }
+    
+    public void drive(int distance){
+        
+    }
+    
+    
+    
+}
